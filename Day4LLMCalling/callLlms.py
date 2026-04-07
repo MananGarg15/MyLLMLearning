b3977f810a3cb5a259fc03ccc9314243a1655baf
PGCP-AI <PGCP-AI@students.acts.in>
2026-04-07 21:34:41 +0530

    removed redundant model functions

--- Day4LLMCalling/callLlms.py
+++ Day4LLMCalling/callLlms.py
@@ -8,11 +8,7 @@ load_dotenv(override=True)
 
 
 class Llms:
-    llama = OpenAI(base_url='http://localhost:11434/v1',api_key='')
-    gpt_oss = OpenAI(base_url='http://localhost:11434/v1',api_key='')
     gemini = OpenAI(base_url="https://generativelanguage.googleapis.com/v1beta/openai/",api_key=os.getenv('GOOGLE_API_KEY'))
-    qwen = OpenAI(base_url="https://openrouter.ai/api/v1",api_key=os.getenv('OPENROUTER_API_KEY'))
-
     openRouter = OpenAI(base_url="https://openrouter.ai/api/v1",api_key=os.getenv('OPENROUTER_API_KEY'))
     ollama = OpenAI(base_url='http://localhost:11434/v1',api_key='')
 
@@ -85,47 +81,7 @@ class Llms:
         if markdown: 
             return Markdown(response.choices[0].message.content)
         return result
-
-    def callQwen(message,new=False,system_prompt = '', response_format='text', markdown=False):
-
-        if isinstance(message,str):
-            messages = mSeries.QwenMessageSeries(message=[{'role':'user','content':message}],new=new)
-        else:
-            messages = mSeries.QwenMessageSeries(message=message,new=new)
-        if system_prompt:
-            system_message = {'role':'system','content':system_prompt}
-            mSeries.qwenMessages[0] = system_message
-
-        response = Llms.qwen.chat.completions.create(model='qwen/qwen3.6-plus:free',messages=messages, response_format={"type":response_format}) 
-        result = response.choices[0].message.content
-
-        newMessage = [{'role':'assistant','content':result}]
-        mSeries.QwenMessageSeries(newMessage)
-
-        if markdown: 
-            return Markdown(response.choices[0].message.content)
-        return result
-
-    def callGPT_OSS(message,new=False,system_prompt = '', response_format='text', markdown=False):
-
-        if isinstance(message,str):
-            messages = mSeries.GPT_OSSMessageSeries(message=[{'role':'user','content':message}],new=new)
-        else:
-            messages = mSeries.GPT_OSSMessageSeries(message=message,new=new)
-        if system_prompt:
-            system_message = {'role':'system','content':system_prompt}
-            mSeries.gpt_ossMessages[0] = system_message
-
-        response = Llms.gpt_oss.chat.completions.create(model='gpt-oss',messages=messages, response_format={"type":response_format}) 
-        result = response.choices[0].message.content
-
-        newMessage = [{'role':'assistant','content':result}]
-        mSeries.GPT_OSSMessageSeries(newMessage)
-
-        if markdown: 
-            return Markdown(response.choices[0].message.content)
-        return result
-        
+             
 
 if __name__=='__main__':
 
--- Day4LLMCalling/messageSeries.py
+++ Day4LLMCalling/messageSeries.py
@@ -1,8 +1,5 @@
 class mSeries:
-    LlamaMessages = [{'role':'system','content':''}]
     geminiMessages = [{'role':'system','content':''}]
-    qwenMessages = [{'role':'system','content':''}]
-    gpt_ossMessages = [{'role':'system','content':''}]
     openRouterModelMessages = {}
     ollamaMessages = {}
 
@@ -28,27 +25,6 @@ class mSeries:
             mSeries.ollamaMessages[model].extend(message)
             return mSeries.ollamaMessages[model]
 
-
-    @staticmethod
-    def LlamaMessageSeries(message,new = False):
-        if(new):
-            initial_message = [{'role':'system','content':''}]
-            mSeries.LlamaMessages = initial_message + message
-            return mSeries.LlamaMessages
-        else:
-            mSeries.LlamaMessages.extend(message)
-            return mSeries.LlamaMessages
-
-    @staticmethod
-    def GPT_OSSMessageSeries(message,new = False):
-        if(new):
-            initial_message = [{'role':'system','content':''}]
-            mSeries.gpt_ossMessages = initial_message + message
-            return mSeries.gpt_ossMessages
-        else:
-            mSeries.gpt_ossMessages.extend(message)
-            return mSeries.gpt_ossMessages
-
     def GeminiMessageSeries(message,new = False):
         if(new):
             initial_message = [{'role':'system','content':''}]
@@ -57,15 +33,6 @@ class mSeries:
         else:
             mSeries.geminiMessages.extend(message)
             return mSeries.geminiMessages
-    
-    def QwenMessageSeries(message,new = False):
-        if(new):
-            initial_message = [{'role':'system','content':''}]
-            mSeries.qwenMessages = initial_message + message
-            return mSeries.qwenMessages
-        else:
-            mSeries.qwenMessages.extend(message)
-            return mSeries.qwenMessages
 
 
 if __name__ == '__main__':
