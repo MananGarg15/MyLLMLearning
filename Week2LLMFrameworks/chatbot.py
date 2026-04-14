477725b40dd7dc12ed59770cc8a3c417cb15ad3a
MananGarg15 <manangarg1535@gmail.com>
2026-04-14 20:58:00 +0530

    added chatbot.py
    also fixed minor chat_history update

---
+++ Week2LLMFrameworks/chatbot.py
@@ -0,0 +1,151 @@
+import gradio as gr
+import PyPDF2
+from Day4LLMCalling.callLlms import Llms, mSeries
+
+def wrapLlm(message,file_content,source,model,temperature,chat_no):
+    if file_content:
+        message += f'File content: {file_content}'
+    yield from Llms.callModelGenerator(message,source=source, model=model, temperature=temperature, chat_no=chat_no)
+
+def update_chatbox(model,chat_no):
+    return mSeries.promptList.get(chat_no,{}).get(model,[])[:-1]
+
+def create_new_chat(chat_list):
+    chat_list.append(f'Chat{len(chat_list)+1}')
+    return chat_list, len(chat_list)
+
+def load_chat(chat_num):
+    return chat_num
+
+def getFileContent(file_path):
+    # For PDF
+    if file_path.name.endswith('.pdf'):
+        with open(file_path.name, 'rb') as f:
+            reader = PyPDF2.PdfReader(f)
+            content = ""
+            for page in reader.pages:
+                content += page.extract_text()
+        return content
+
+    if file_path.name.endswith('.txt'):
+        with open(file_path.name, 'r') as f:
+            content = f.read()
+        return content
+
+def startApp():
+
+    with gr.Blocks(theme=gr.themes.Soft()) as demo:
+        chat_list = gr.State(['Chat1']) 
+        chat_no = gr.State(1)
+        source = gr.State('openRouter')
+        model_name = gr.State('openrouter/free')
+        temperature = gr.State(0)
+        file_content = gr.State('')
+        
+        with gr.Row():
+            with gr.Column(scale=1, variant='panel'):
+
+                new_chat_btn = gr.Button('New',variant='huggingface',size='sm')
+
+                @gr.render(inputs=[chat_list])
+                def render_chats(chat_list):
+                    with gr.Group():
+                        for i, chat in enumerate(chat_list) :
+                            chat_num = gr.State(i+1)
+                            btn = gr.Button(chat,size='lg', variant= 'stop')
+
+                            btn.click(
+                                fn=load_chat, 
+                                inputs=[chat_num],
+                                outputs=[chat_no]
+                            ).then(
+                                fn=update_chatbox, 
+                                inputs=[model_name,chat_no], 
+                                outputs=[chat_history] )
+
+                new_chat_btn.click(
+                    fn = create_new_chat, 
+                    inputs=[chat_list], 
+                    outputs=[chat_list,chat_no] )
+
+                
+            with gr.Column(scale=4):
+
+                chat_history = gr.Chatbot(label='Chat History')    
+                response_box = gr.Markdown(label='Last response')
+                    
+                with gr.Group():    
+                    user_input = gr.Textbox(
+                        placeholder='Enter your prompt',
+                        show_label=False,
+                        scale=8)
+                    submit_btn= gr.Button(
+                        'enter',
+                        size='sm',
+                        variant='primary',
+                        scale=1)
+
+                submit_btn.click(
+                    wrapLlm,
+                    inputs=[user_input, file_content,source,model_name,temperature,chat_no], 
+                    outputs=[response_box]
+                ).then(
+                    fn=update_chatbox, 
+                    inputs=[model_name,chat_no], 
+                    outputs=chat_history
+                ).then(
+                    fn = lambda rst='':rst, 
+                    outputs=[user_input] )
+
+                user_input.submit(
+                    wrapLlm,
+                    inputs=[user_input, file_content,source,model_name,temperature,chat_no], 
+                    outputs=[response_box]
+                ).then(
+                    fn=update_chatbox, 
+                    inputs=[model_name,chat_no], 
+                    outputs=chat_history
+                ).then(
+                    fn = lambda rst='':rst,
+                    outputs=[user_input])
+
+
+            with gr.Column(scale=1):
+                with gr.Accordion('Adv_settings'):
+                    source_selection = gr.Dropdown(
+                        choices=['openRouter','gemini','ollama'],
+                        label='Select source-')
+
+                    source_selection.change(
+                        fn= lambda source:source, 
+                        inputs=[source_selection],
+                        outputs= [source])
+
+                    temperature_select = gr.Slider(
+                        0,2,value=0,
+                        step=0.1,
+                        label='temp_slider') 
+
+                    model_name_input = gr.Textbox(
+                        placeholder='Enter model name',
+                        value='openrouter/free',
+                        label='Model name')
+                    
+                    model_name_input.submit(
+                        fn = lambda model_name:model_name,
+                        inputs=[model_name_input],
+                        outputs=[model_name])
+
+                    temperature_select.change(
+                        fn = lambda temperature:temperature, 
+                        inputs=[temperature_select],
+                        outputs=[temperature])
+
+            
+                files = gr.File(label='insert file',file_count='single',file_types=['.pdf','.txt'])  
+                files.upload(fn = getFileContent, inputs=[files], outputs=[file_content] )
+
+    return demo
+
+if __name__ == '__main__':
+    print('hi')
