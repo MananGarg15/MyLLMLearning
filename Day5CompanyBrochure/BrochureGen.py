20b028d598d19ce33dbf8819161c854802a08e89
MananGarg15 <manangarg1535@gmail.com>
2026-04-14 21:41:24 +0530

    created Brochure App
    created file BrochureGen for the app.

---
+++ Day5CompanyBrochure/BrochureGen.py
@@ -0,0 +1,31 @@
+from re import L
+from Day5CompanyBrochure.BrochureContent import getBrochureContent
+from Day4LLMCalling.callLlms import Llms
+
+def getBrochure(url, company_name = '', stream=True, source = 'ollama'):
+    brochure_system_prompt = """
+    You are a professional business writer. Your goal is to analyze the provided company website content and summarize
+     it into a short brochure for customers, investors, and potential recruits.
+
+Instructions:
+
+Tone: Professional and engaging.
+
+Structure: Use clear sections for "Company Overview," "Products & Services," "Investor Highlights," "Careers & Culture," or any other titles you may find relevant
+
+Formatting: Use Markdown with bold headers and bullet points. Do not use code blocks.
+
+Constraint: Keep the total length concise (around 800-1000 words).
+    """
+
+    brochure_user_prompt = f"""
+    The name of the company is {company_name}. Here are the contents - 
+
+    """
+    contents = getBrochureContent(url, source=source)[:5000]
+    brochure_user_prompt += contents
+    
+
+    print(f'Creating brochure for {company_name}...')
+
+    yield from Llms.callModelGenerator(brochure_user_prompt,system_prompt=brochure_system_prompt,markdown=True,stream=stream,source=source)
--- Week2LLMFrameworks/__init__.py
+++ Week2LLMFrameworks/__init__.py
@@ -1,1 +1,2 @@
 from .chatbot import startApp
+from .BrochureBot import startBrochureApp
